ef3b2be5348d55c5ef44de4a8fd23bbed9e17a69
Kash <alhilfihussein25@gmail.com>
2023-01-03 23:51:41 +0100

    rgdfg

--- src/components/Home/home2.vue
+++ src/components/Home/home2.vue
@@ -7,18 +7,18 @@
       style="margin-bottom: 20px; display: flex; justify-content: space-between"
     >
       <div class="cont">
-        <img src="../../assets/icons/3G 1.png" alt="" />
+        <img src="../../assets/icons/3G.png" alt="3Gaming أبوعابد" />
         <p>3Gaming أبوعابد</p>
       </div>
-      <div class="cont"> <img src="../../assets/icons/Ocmz 1.png" alt="" /><p>oCMz أوسمز</p></div>
-      <div class="cont"> <img src="../../assets/icons/opiilz 1.png" alt="" /><p>صالح oPiiLz</p></div>
-      <div class="cont"> <img src="../../assets/icons/Aziz 1.png" alt="" /><p>Aziz عزيز</p></div>
+      <div class="cont"> <img src="../../assets/icons/Ocmz.png" alt="" /><p>oCMz أوسمز</p></div>
+      <div class="cont"> <img src="../../assets/icons/opiilz.png" alt="" /><p>صالح oPiiLz</p></div>
+      <div class="cont"> <img src="../../assets/icons/Aziz.png" alt="" /><p>Aziz عزيز</p></div>
     </div>
     <div style="display: flex; justify-content: space-between">
-      <div class="cont"> <img src="../../assets/icons/TeamFalcons - V2A 1.png" alt="" /><p>Falcons فالكونز</p></div>
-      <div class="cont"> <img src="../../assets/icons/Bandar 1.png" alt="" /><p>BanderitaX</p></div>
-      <div class="cont"> <img src="../../assets/icons/Xsma 1.png" alt="" /><p>xSMA333</p></div>
-      <div class="cont"> <img src="../../assets/icons/Frame 32.png" alt="" /><p>Ahmed Show</p></div>
+      <div class="cont"> <img src="../../assets/icons/TeamFalcons.png" alt="" /><p>Falcons فالكونز</p></div>
+      <div class="cont"> <img src="../../assets/icons/Bandar.png" alt="" /><p>BanderitaX</p></div>
+      <div class="cont"> <img src="../../assets/icons/Xsma.png" alt="" /><p>xSMA333</p></div>
+      <div class="cont"> <img src="../../assets/icons/Frame.png" alt="" /><p>Ahmed Show</p></div>
     </div>
   </div>
 </template>
